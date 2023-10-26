f98df216043fa3d75b7ad7abc02a05ac288af0c6
mikenerly <mikenerly@gmail.com>
2023-10-26 16:31:52 -0400

    another commit for master to try to fix welcome_good page

--- resources/views/welcome_good.blade.php
+++ resources/views/welcome_good.blade.php
@@ -2,17 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Eglise Baptiste de Petion-Ville</title>
-
-    <!--CSS link local -->
-        <link rel="stylesheet" href="{{ asset('css/custom_good.css') }}">
-    <!--Boxicons CSS -->
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <title>Eglise Evangelique Baptiste de Petion-Ville</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <!--CSS external-->
+    <link rel="stylesheet" href="{{ asset('css/custom_good.css') }}"> 
+    <!--Boxicom css-->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    
 </head>
 <body>
     <header id="header-wraper">
@@ -54,19 +54,7 @@
                         </div>
                     </div>
                 </div>
-    <div class="container">
-        <div class="row top-wrapper">
-            <div class="header-top-1 col-6">
-                <ul class="top-menu-list">  
-                    <li class="menu-item-1">#569, Route de Delmas ( entre Delmas 91 et 93), Delmas, Haiti</li>
-                </ul>
             </div>
-            <div class="header-top-2 col-6">
-                <ul class="top-menu-list">  
-                    <li class="menu-item-2">A Propos</li>
-                    <li class="menu-item-3">Contactez Nous</li>
-                </ul>
-            </div> 
         </div>
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container">
